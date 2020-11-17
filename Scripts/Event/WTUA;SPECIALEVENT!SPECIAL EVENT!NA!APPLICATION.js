4d957728adeaab91ff78c6cf9bf2a02329a22fb9
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2020-11-16 16:37:59 -0800

    update

--- Scripts/Event/WTUA;SPECIALEVENT!SPECIAL EVENT!NA!APPLICATION.js
+++ Scripts/Event/WTUA;SPECIALEVENT!SPECIAL EVENT!NA!APPLICATION.js
@@ -1,23 +1,20 @@
 //WTUA;SPECIALEVENT!SPECIAL EVENT!NA!APPLICATION
 if ((AInfo["Stage"] == "CHECKED") 
     && (wfTask == "Application Submittal" && wfStatus == "Department Review")){
-        activateTask("Comm Dev Bldg Safety","Pending","Auto Assigned by Script","Auto Assigned by Script");
-        logDebug("County Assessor permit issuance email");
-		//Get Report and Report Parameters
-              
+        activateTask("Comm Dev Bldg Safety","Pending");
+        //logDebug("Department Notification email");
+		//Get Email Notification and Parameters
 	        var fromEmail = lookup("SCRIPT_EMAIL_FROM", "AGENCY_FROM");
                 var toEmail = "jason@grayquarter.com";
                 var ccEmail = ""; //blank for now
-                //var theURL = "https://landuse.santabarbaraca.gov/CitizenAccess";
                 var emailParameters = aa.util.newHashtable();
 	            addParameter(emailParameters, "$$altID$$", cap.getCapModel().getAltID());
                 addParameter(emailParameters, "$$recordAlias$$", cap.getCapType().getAlias());
-                //addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(theURL));
-                
+                                
                 var emailTemplate = "SE_ASSIGN_DEPART_TASK_NOTICE";
                 var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
                 var fileNames = [];
                
                 aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, ccEmail, emailTemplate, emailParameters, capId4Email, fileNames);
                 logDebug( ": Sent Email template " + emailTemplate + " To Contacts ");
-}  
+}
