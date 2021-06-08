a78eb1a74725f5f8d1d3f5f630b430725d78b494
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-06-08 13:33:47 -0700

    Update

--- Scripts/Event/IRSA;BUILDING!OVER THE COUNTER!~!~.js
+++ Scripts/Event/IRSA;BUILDING!OVER THE COUNTER!~!~.js
@@ -5,12 +5,15 @@
 
 //GQ - Zen Desk Ticket #1728
 logDebug("start of IRSA;BUILDING!OVER THE COUNTER!~!~");
-var inspection = ('Building Final - Not Occupied','Final Electrical','Final Mechanical','Final Plumbing');
-if (inspType == inspection && inspResult == "Passed") {
-	logDebug("Criteria met at inspection type of " + inspType);
+
+if ((inspType == "Building Final - Not Occupied" 
+	|| inspType == "Final Electrical"
+	|| inspType == "Final Mechanical"
+	|| inspType == "Final Plumbing")
+	&& inspResult == "Passed") {
 	updateTask("Inspection","Final Inspection Complete","Auto Closed by Script","Auto Closed by Script");
 	//closeTask("Close","Closed","Auto Closed by Script","Auto Closed by Script");
 	updateAppStatus("Closed","Auto Closed by Script");
 	}
-
+	logDebug("Criteria met at inspection type of " + inspType);
 logDebug("end of IRSA;BUILDING!OVER THE COUNTER!~!~");
