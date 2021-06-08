182cf1152fe1dd8636a2de13e6be8628f5ecd4a0
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-06-08 13:00:42 -0700

    New script for OnDemand

---
+++ Scripts/Event/IRSA;BUILDING!OVER THE COUNTER!~!~.js
@@ -0,0 +1,16 @@
+//********************************************************************************************************
+//Inspection Result Submit After for OnDemand Records.
+//Created By: Gray Quarter Inc. 6/8/21
+//********************************************************************************************************
+
+//GQ - Zen Desk Ticket #1728
+logDebug("start of IRSA;BUILDING!OVER THE COUNTER!~!~");
+var inspection = ('Building Final - Not Occupied','Final Electrical','Final Mechanical','Final Plumbing');
+if (inspType == inspection && inspResult == "Passed") {
+	logDebug("Criteria met at inspection type of " + inspType);
+	closeTask("Inspection","Final Inspection Passed","Auto Closed by Script","Auto Closed by Script");
+	closeTask("Close","Closed","Auto Closed by Script","Auto Closed by Script");
+	updateAppStatus("Closed","Auto Closed by Script");
+	}
+
+logDebug("end of IRSA;BUILDING!OVER THE COUNTER!~!~");
