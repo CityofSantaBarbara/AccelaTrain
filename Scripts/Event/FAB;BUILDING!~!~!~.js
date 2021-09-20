7e81195bb99f0caf943bd21ec1e39d94a5b0b883
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-09-20 09:56:59 -0700

    Update for NRS Fees

---
+++ Scripts/Event/FAB;BUILDING!~!~!~.js
@@ -0,0 +1,24 @@
+logDebug("start FAB;BUILDING!~!~!~ to check correct fee!");
+
+for (var fiIndex in FeeItemsList) {
+    thisFeeItem = FeeItemsList[fiIndex];
+    if (thisFeeItem == 'BLD_NRS_10') {
+        feeQty = FeeItemsQuantityList[fiIndex];
+        if (parseFloat(feeQty) > 1000) {
+            cancel = true;
+            showMessage= true;
+            comment("You have selected a sq ft over 1000 sq ft. Please use fee New R3 Residence Plan Check Fee (Over 1000 sq ft)");
+        }
+    }
+    if (thisFeeItem == 'BLD_NRS_010') {
+        feeQty = FeeItemsQuantityList[fiIndex];
+        if (parseFloat(feeQty) < 1000) {
+            cancel = true;
+            showMessage= true;
+            comment("You have selected a sq ft under 1000 sq ft. Please use fee New R3 Residence Plan Check Fee (Under 1000 sq ft)");
+        }
+    }
+}
+
+logDebug("end  FAB;BUILDING!~!~!~ to check correct fee!");
+
