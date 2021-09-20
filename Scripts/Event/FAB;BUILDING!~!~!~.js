8e3b1993e2a6200e735aa8b3299df0899e12d643
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-09-20 10:02:26 -0700

    Update

--- Scripts/Event/FAB;BUILDING!~!~!~.js
+++ Scripts/Event/FAB;BUILDING!~!~!~.js
@@ -4,7 +4,7 @@ for (var fiIndex in FeeItemsList) {
     thisFeeItem = FeeItemsList[fiIndex];
     if (thisFeeItem == 'BLD_NRS_10') {
         feeQty = FeeItemsQuantityList[fiIndex];
-        if (parseFloat(feeQty) > 1000) {
+        if (parseFloat(feeQty) >= 1000) {
             cancel = true;
             showMessage= true;
             comment("You have selected a sq ft over 1000 sq ft. Please use fee New R3 Residence Plan Check Fee (Over 1000 sq ft)");
