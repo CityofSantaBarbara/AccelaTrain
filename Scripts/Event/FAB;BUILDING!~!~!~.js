7d12ac8bbb4fa1802ec91d9eaf6a1414884c8c5c
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-09-20 10:03:23 -0700

    Update

--- Scripts/Event/FAB;BUILDING!~!~!~.js
+++ Scripts/Event/FAB;BUILDING!~!~!~.js
@@ -7,7 +7,7 @@ for (var fiIndex in FeeItemsList) {
         if (parseFloat(feeQty) >= 1000) {
             cancel = true;
             showMessage= true;
-            comment("You have selected a sq ft over 1000 sq ft. Please use fee New R3 Residence Plan Check Fee (Over 1000 sq ft)");
+            comment("You have selected a sq ft over 1000 sq ft. Please use the fee that  is (Over 1000 sq ft)");
         }
     }
     if (thisFeeItem == 'BLD_NRS_010') {
@@ -15,7 +15,7 @@ for (var fiIndex in FeeItemsList) {
         if (parseFloat(feeQty) < 1000) {
             cancel = true;
             showMessage= true;
-            comment("You have selected a sq ft under 1000 sq ft. Please use fee New R3 Residence Plan Check Fee (Under 1000 sq ft)");
+            comment("You have selected a sq ft under 1000 sq ft. Please use the fee that  is (Under 1000 sq ft)");
         }
     }
 }
