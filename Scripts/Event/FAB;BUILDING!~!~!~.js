b1c8275c8ba861de1d747be1dec09ee4ae3f268c
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-09-20 11:50:37 -0700

    Update

--- Scripts/Event/FAB;BUILDING!~!~!~.js
+++ Scripts/Event/FAB;BUILDING!~!~!~.js
@@ -8,16 +8,25 @@ for (var fIndex in feeItemArr) {
     thisFeeItem = String(feeItemArr[fIndex]);
     logDebug(thisFeeItem);
     if (thisFeeItem == 'BLD_NRS_10') {
-        logDebug("First IF Statement");
         newQtyList =  String(FeeItemsQuantityList).replace("[","").replace("]","");  
         newQtyList = newQtyList + "";
         feeQtyArray = newQtyList.split("|");
         feeQty = feeQtyArray[fIndex];
         if (parseFloat(feeQty) >= 1000) {
-            logDebug("Second IF Statement");
             cancel = true;
             showMessage= true;
             comment("You have selected a sq ft over 1000 sq ft. Please use the fee that  is (Over 1000 sq ft)");
+            }
+        }
+    if (thisFeeItem == 'BLD_NRS_010') {
+        newQtyList =  String(FeeItemsQuantityList).replace("[","").replace("]","");  
+        newQtyList = newQtyList + "";
+        feeQtyArray = newQtyList.split("|");
+        feeQty = feeQtyArray[fIndex];
+        if (parseFloat(feeQty) < 1000) {
+            cancel = true;
+            showMessage= true;
+            comment("You have selected a sq ft Under 1000 sq ft. Please use the fee that is (Under 1000 sq ft)");
             }
         }
 }
