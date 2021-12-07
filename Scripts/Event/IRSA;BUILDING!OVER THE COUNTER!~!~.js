a727718f876d9b6671dfb13091b1f2ba2006cfce
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-12-07 08:51:35 -0800

    Update

--- Scripts/Event/IRSA;BUILDING!OVER THE COUNTER!~!~.js
+++ Scripts/Event/IRSA;BUILDING!OVER THE COUNTER!~!~.js
@@ -14,7 +14,30 @@ if ((inspType == "Building Final - Not Occupied"
 	closeTask("Inspection","Final Inspection Complete","Auto Closed by Script","Auto Closed by Script");
 	//closeTask("Close","Closed","Auto Closed by Script","Auto Closed by Script");
 	updateAppStatus("Complete","Auto Closed by Script");
-	runAsyncEvent("ASYNC_INSP_SUMMARY_REPORT_SEND_EMAIL",capIDString,currentUserID);
-	}
+	//runAsyncEvent("ASYNC_INSP_SUMMARY_REPORT_SEND_EMAIL",capIDString,currentUserID);
+
+
+	logDebug("County Assessor email");
+	//Get Report and Report Parameters
+	  
+	var fromEmail = lookup("SCRIPT_EMAIL_FROM", "AGENCY_FROM");
+	var toEmail = "jason@grayquarter.com";
+	var ccEmail = "jason@grayquarter.com"; //blank for now
+	  //var toEmail = "citypermits@co.santa-barbara.ca.us";
+	  //var ccEmail = "CDRecords@SantaBarbaraCA.gov"; //blank for now
+		var theURL = "https://landuse.santabarbaraca.gov/CitizenAccess";
+		var emailParameters = aa.util.newHashtable();
+	addParameter(emailParameters, "$$altID$$", cap.getCapModel().getAltID());
+		addParameter(emailParameters, "$$recordAlias$$", cap.getCapType().getAlias());
+		addParameter(emailParameters, "$$acaRecordUrl$$", getACARecordURL(theURL));
+	
+		var emailTemplate = "BLD_PERMIT_FINAL_INSPECTION_APP_ASSESSOR";
+		var capId4Email = aa.cap.createCapIDScriptModel(capId.getID1(), capId.getID2(), capId.getID3());
+		var fileNames = [];
+	   
+		aa.document.sendEmailAndSaveAsDocument(fromEmail, toEmail, ccEmail, emailTemplate, emailParameters, capId4Email, fileNames);
+		logDebug( ": Sent Email template " + emailTemplate + " To Contacts ");
+
+}
 	logDebug("Criteria met at inspection type of " + inspType);
 logDebug("end of IRSA;BUILDING!OVER THE COUNTER!~!~");
