1460cc28981aaba76b48a3f4c92909ae5cf0ec07
jason-grayquarter <64280334+jason-grayquarter@users.noreply.github.com>
2021-12-23 16:46:14 -0800

    New for Documents

---
+++ Scripts/Event/DUB;BUILDING!~!~!~.js
@@ -0,0 +1,25 @@
+//DUB;BUILDING!~!~!~.js
+//DUB:BUILDING/*/*/*
+//Added by Gray Quarter
+//This script is to make sure there is a Document Group, Document Category, & Description
+
+var docArray = documentModelArray.toArray();
+var errors = [];
+if(docArray && docArray.length > 0) {
+    for(var z in docArray) {
+        var docModel = docArray[z];
+        var docName = docModel.fileName;
+        var category = docModel.docCategory;
+        var description = docModel.docDescription;
+        if(!category || !description) {
+            errors.push(docName + " is missing a category and/or description.");
+        }
+    }
+}
+
+if(errors.length > 0) {
+    showMessage = true;
+    comment(errors.join("<br>"));
+    comment("Please fill out before you continue your upload.");
+    cancel = true;
+}
